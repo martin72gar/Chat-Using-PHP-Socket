42dc77d1437583dd3f269f2566d25c11cb38c4a7
Office PC <72garmartin@gmail.com>
2022-01-13 17:59:04 +0700

    adding socket example

---
+++ php-to-android-client/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'baa5b61a30de9cde353bb3e470ac6e1f2c49e885',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'baa5b61a30de9cde353bb3e470ac6e1f2c49e885',
+            'dev_requirement' => false,
+        ),
+        'workerman/workerman' => array(
+            'pretty_version' => 'v4.0.26',
+            'version' => '4.0.26.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../workerman/workerman',
+            'aliases' => array(),
+            'reference' => '27573e9f985f9ec0665b1f9924308d359bd0fdaa',
+            'dev_requirement' => false,
+        ),
+    ),
+);
